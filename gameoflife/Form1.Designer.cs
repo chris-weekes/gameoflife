8892f53d0b53765a91804fb5a6f359892fded2b7
Chris Weekes <cweekes@asptek.ca>
2021-02-12 13:17:55 -0500

    Add project files.

---
+++ gameoflife/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace gameoflife
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.gameToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.startToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.stopToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.rpentominoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.gliderGunToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.infinteToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.gameToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(938, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // gameToolStripMenuItem
+            // 
+            this.gameToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.startToolStripMenuItem,
+            this.stopToolStripMenuItem,
+            this.rpentominoToolStripMenuItem,
+            this.gliderGunToolStripMenuItem,
+            this.infinteToolStripMenuItem});
+            this.gameToolStripMenuItem.Name = "gameToolStripMenuItem";
+            this.gameToolStripMenuItem.Size = new System.Drawing.Size(50, 20);
+            this.gameToolStripMenuItem.Text = "Game";
+            // 
+            // startToolStripMenuItem
+            // 
+            this.startToolStripMenuItem.Name = "startToolStripMenuItem";
+            this.startToolStripMenuItem.Size = new System.Drawing.Size(98, 22);
+            this.startToolStripMenuItem.Text = "Start";
+            this.startToolStripMenuItem.Click += new System.EventHandler(this.startToolStripMenuItem_Click);
+            // 
+            // stopToolStripMenuItem
+            // 
+            this.stopToolStripMenuItem.Name = "stopToolStripMenuItem";
+            this.stopToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.stopToolStripMenuItem.Text = "Stop";
+            this.stopToolStripMenuItem.Click += new System.EventHandler(this.stopToolStripMenuItem_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 24);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(938, 561);
+            this.panel1.TabIndex = 1;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // rpentominoToolStripMenuItem
+            // 
+            this.rpentominoToolStripMenuItem.Name = "rpentominoToolStripMenuItem";
+            this.rpentominoToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.rpentominoToolStripMenuItem.Text = "r-pentomino";
+            this.rpentominoToolStripMenuItem.Click += new System.EventHandler(this.rpentominoToolStripMenuItem_Click);
+            // 
+            // gliderGunToolStripMenuItem
+            // 
+            this.gliderGunToolStripMenuItem.Name = "gliderGunToolStripMenuItem";
+            this.gliderGunToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.gliderGunToolStripMenuItem.Text = "glider gun";
+            this.gliderGunToolStripMenuItem.Click += new System.EventHandler(this.gliderGunToolStripMenuItem_Click);
+            // 
+            // infinteToolStripMenuItem
+            // 
+            this.infinteToolStripMenuItem.Name = "infinteToolStripMenuItem";
+            this.infinteToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.infinteToolStripMenuItem.Text = "infinte";
+            this.infinteToolStripMenuItem.Click += new System.EventHandler(this.infinteToolStripMenuItem_Click);
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.WorkerSupportsCancellation = true;
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(938, 585);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Game of Life";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem gameToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem startToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem stopToolStripMenuItem;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ToolStripMenuItem rpentominoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem gliderGunToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem infinteToolStripMenuItem;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+    }
+}
+
---
+++ gameoflife/Form1.cs
@@ -0,0 +1,260 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace gameoflife
+{
+    public partial class Form1 : Form
+    {
+        int mX = 500;
+        int mY = 250;
+        int scale=3;
+
+        SolidBrush BrushBlack;
+        SolidBrush BrushWhite;
+        BufferedGraphicsContext currentContext;
+        BufferedGraphics myBuffer;
+
+        int[,] boardCache;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void startToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+
+           // this.DoubleBuffered = true;
+
+            int[,] board = Init();
+            
+            Random r=new Random();
+            //create random board
+            for (int x = 0; x < mX; x++)
+            {
+                for (int y = 0; y < mY; y++)
+                {
+
+                    board[x, y] = r.Next(0, 2);
+                }
+            }
+
+            Run(board);
+        }
+
+        private void Run(int[,] board)
+        {
+            
+
+            boardCache = board;
+
+            DrawBoard(boardCache);
+
+            backgroundWorker1.RunWorkerAsync();
+
+        }
+
+        private int[,] Init()
+        {
+            currentContext = BufferedGraphicsManager.Current;
+            myBuffer = currentContext.Allocate(panel1.CreateGraphics(), panel1.DisplayRectangle);
+
+            BrushBlack = new SolidBrush(Color.DarkGreen);
+            BrushWhite = new SolidBrush(Color.WhiteSmoke);
+
+            int[,] board = new int[mX, mY];
+            return board;
+        }
+
+        public void DrawBoard( int[,] b)
+        {
+            myBuffer.Graphics.Clear(Color.WhiteSmoke);
+         
+
+            for (int x = 0; x < mX; x++)
+            {
+                for (int y = 0; y < mY; y++)
+                {
+                    if (b[x, y] > 0)
+                    {
+                        myBuffer.Graphics.FillRectangle(BrushBlack, x * scale, y * scale, scale, scale);
+                    }
+                    //else
+                    //{
+                    //  //  myBuffer.Graphics.FillRectangle(BrushWhite, x * 5, y * 5, 5, 5); 
+                    //}
+
+                }
+            }
+
+            myBuffer.Render();
+            
+        }
+
+        public void Generate(int[,] b)
+        {
+            int[,] board = new int[mX, mY];
+            int t = 0;
+            for (int x = 1; x < mX-1; x++)
+            {
+                for (int y = 1; y < mY-1; y++)
+                {
+                    t = b[x - 1, y - 1];
+                    t += b[x - 1, y ];
+                    t += b[x - 1, y+1];
+                    t += b[x , y-1];
+                    t += b[x , y+1];
+                    t += b[x + 1, y-1];
+                    t += b[x + 1, y];
+                    t += b[x + 1, y+1];
+
+                    if (b[x, y] == 0 & t == 3)
+                        board[x, y] = 1;
+                    else if(b[x,y]==1 & (t==2 | t==3))
+                        board[x, y] = 1;
+                }
+            }
+
+            boardCache = board;
+            //panel1.Refresh();
+            DrawBoard( boardCache);
+            
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            if (boardCache != null)
+            {
+                //e.Graphics.Clear(Color.White);
+
+                //DrawBoard(e.Graphics, boardCache);
+
+                
+            }
+        }
+
+        private void rpentominoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            int[,] board = Init();
+
+            board[100, 100] = 1;
+            board[101, 100] = 1;
+            board[101, 99] = 1;
+            board[102, 99] = 1;
+            board[101, 101] = 1;
+
+            Run(board);
+        }
+
+        private void gliderGunToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            int[,] board = Init();
+
+            board[100, 100] = 1;
+            board[101, 100] = 1;
+            board[100, 101] = 1;
+            board[101, 101] = 1;
+
+            board[110, 100] = 1;
+            board[110, 101] = 1;
+            board[110, 102] = 1;
+
+            board[111, 99] = 1;
+            board[111, 103] = 1;
+
+            board[112, 98] = 1;
+            board[112, 104] = 1;
+            board[113, 98] = 1;
+            board[113, 104] = 1;
+
+            board[114, 101] = 1;
+
+            board[115, 99] = 1;
+            board[115, 103] = 1;
+
+            board[116, 100] = 1;
+            board[116, 101] = 1;
+            board[116, 102] = 1;
+
+            board[117, 101] = 1;
+
+            board[120, 98] = 1;
+            board[120, 99] = 1;
+            board[120, 100] = 1;
+
+            board[121, 98] = 1;
+            board[121, 99] = 1;
+            board[121, 100] = 1;
+
+            board[122, 97] = 1;
+            board[122, 101] = 1;
+
+            board[124, 97] = 1;
+            board[124, 96] = 1;
+            board[124, 102] = 1;
+            board[124, 101] = 1;
+
+            board[134, 98] = 1;
+            board[134, 99] = 1;
+            board[135, 98] = 1;
+            board[135, 99] = 1;
+            
+            Run(board);
+        }
+
+        private void infinteToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            int[,] board = Init();
+
+            board[99, 100] = 1;
+            
+            board[101, 100] = 1;
+            board[101, 99] = 1;
+            
+            board[103, 98] = 1;
+            board[103,97] = 1;
+            board[103, 96] = 1;
+
+            board[105, 97] = 1;
+            board[105, 96] = 1;
+            board[105, 95] = 1;
+
+            board[106, 96] = 1;
+
+         
+            Run(board);
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            for (int gen = 0; gen < 10000; gen++)
+            {
+                //  System.Threading.Thread.Sleep(10);
+                if (backgroundWorker1.CancellationPending)
+                {
+                    e.Cancel = true;
+                    return;
+                }
+                else
+                {
+                    Generate(boardCache);
+                }
+            }
+
+        }
+
+        private void stopToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            backgroundWorker1.CancelAsync();
+        }
+
+       
+     
+    }
+}
